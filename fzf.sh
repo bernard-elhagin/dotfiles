5b87e290ca4cc06d194f91c023a2a5c142a80b58
Bernard El-Hagin <bernard.elhagin@gmail.com>
2018-11-21 12:04:20 +0100

    Fixed fzf setup

--- fzf.sh
+++ fzf.sh
@@ -1,13 +1,7 @@
-# Setup fzf
+# Completion
 # ---------
-if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
-      export PATH="$PATH:$HOME/.fzf/bin"
-fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
+source "$HOME/.fzf/completion.zsh"
 
 # Key bindings
 # ------------
-source "$HOME/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/key-bindings.zsh"
