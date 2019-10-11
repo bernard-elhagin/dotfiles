9706d39c47a1cc40335a3c00430c48b636c5c3bb
Bernard El-Hagin <bernard.el-hagin@gmail.com>
2019-10-11 14:32:52 +0200

    Added delete_topics.sh

--- bin/create_topics.sh
+++ bin/create_topics.sh
@@ -24,9 +24,10 @@ filename="$1"
 while IFS='' read -r line || [[ -n "$line" ]]; do
     topic=$(echo "$line" | cut -f 1 -d" ")
     retention=$(echo "$line" | cut -f 2 -d" ")
+    partitions=$(echo "$line" | cut -f 3 -d" ")
 
     retention_ms=$((retention*24*60*60*1000))
 
-    /opt/kafka/bin/kafka-topics.sh --create --zookeeper $zookeeper --replication-factor 3 --partitions 3 --topic "$topic" --config retention.ms=$retention_ms
+    /opt/kafka/bin/kafka-topics.sh --create --zookeeper $zookeeper --replication-factor 3 --partitions $partitions --topic "$topic" --config retention.ms=$retention_ms
     done < "$filename"
 exit 0
