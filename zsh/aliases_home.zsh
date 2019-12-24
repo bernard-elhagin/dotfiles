b3c0720da9792c8841ba61b69d2ce8680f75fcf4
Bernard El-Hagin <bernard.el-hagin@gmail.com>
2019-12-24 10:38:13 +0100

    Replaced ls with exa where available

--- zsh/aliases_home.zsh
+++ zsh/aliases_home.zsh
@@ -22,3 +22,8 @@ alias r='ranger'
 alias zd='z dot'
 
 alias tx='tmuxinator start'
+
+if (command -v exa >/dev/null); then
+    alias ll='exa   -l --header --time-style=long-iso --group-directories-first'
+    alias lll='exa -al --header --time-style=long-iso --group-directories-first'
+fi
